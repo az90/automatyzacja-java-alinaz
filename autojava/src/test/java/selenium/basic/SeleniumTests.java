6bca26998e33cdb9d673ee6fc2b9d4536e14e56a
Alina Zieniewicz <alina.zieniewicz@gmail.com>
2018-12-12 11:56:55 +0100

    test sprawdzanie autora

--- autojava/src/test/java/selenium/basic/SeleniumTests.java
+++ autojava/src/test/java/selenium/basic/SeleniumTests.java
@@ -10,16 +10,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SeleniumTests {
 
     private WebDriver driver;
+
     @BeforeEach
     public void startDriver(){
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(); //tylko deklaracja zmiennej
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @Test
@@ -41,7 +45,28 @@ public class SeleniumTests {
         WebElement result = resultList.get(0);
         Assertions.assertEquals("agileszkolenia.pl/",result.findElement(By.tagName("cite")).getText());
 
-        driver.quit();
+    }
+
+    @Test
+    public void verifyAuthorOfBlogNoteAboutTransformations(){
+       driver.get("http://markowicz.pro/");
+       Stream<WebElement> listOfNotes = driver.findElements(By.className("entry-title"))
+               .stream()
+               .filter(n -> n.findElement(By.tagName("a")).getText().equals("O transformacjach"));
+
+       List<WebElement> filterdNotes = listOfNotes.collect(Collectors.toList());
+
+        //sprawdzenie oczekuje wielkosci kolekcji notek =1
+       Assertions.assertEquals(1, filterdNotes.size(), "Only one matching note is find on main page");
+
+       driver.findElement(By.cssSelector("#eu-cookie-law input")).submit();
+       filterdNotes.get(0).click();
+
+       WebElement author = driver.findElement(By.cssSelector(".author > a"));
+
+       Assertions.assertEquals("Rafał", author.getText(), "Proper author name is display");
+       Assertions.assertEquals("http://markowicz.pro/author/rafal-markowicz/",
+               author.getAttribute("href"), "Valid author URL is display");
 
     }
 
